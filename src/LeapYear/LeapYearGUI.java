c03ab9a9f495b920db5b4c142ce1732d2ec16e34
Le Bronn <lebronn.samson@cit.edu>
2022-12-16 13:21:03 +0800

    Rename LeapYear Components

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -2,8 +2,17 @@ package LeapYear;
 
 import javax.swing.*;
 
-public class LeapYearGUI {
-    private JPanel panel1;
-    private JTextField textField1;
-    private JButton checkButton;
+public class LeapYearGUI extends JFrame {
+    private JPanel pnlMain;
+    private JTextField tfYear;
+    private JButton btnCheckYear;
+
+    public static void main(String[] args) {
+        LeapYearGUI app = new LeapYearGUI();
+        app.setContentPane(app.pnlMain);
+        app.setSize(700, 700);
+        app.setTitle("Leap Year Checker");
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setVisible(true);
+    }
 }
