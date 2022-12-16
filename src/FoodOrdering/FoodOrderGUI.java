739c2580df3a94c23ce470cfd07cc4caa9a5f541
Le Bronn <lebronn.samson@cit.edu>
2022-12-16 18:11:15 +0800

    Add Exception Handling to FoodOrder

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -21,7 +21,7 @@ public class FoodOrderGUI extends JFrame {
     public static void main(String[] args) {
         FoodOrderGUI app = new FoodOrderGUI();
         app.setContentPane(app.pnlMain);
-        app.setSize(700, 700);
+        app.setSize(550, 550);
         app.setTitle("Food Ordering System");
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
         app.setVisible(true);
@@ -31,38 +31,50 @@ public class FoodOrderGUI extends JFrame {
         btnOrder.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                double order = 0;
-                if (cPizza.isSelected()) {
-                    order += 100;
+                try {
+                    double order = 0;
+                    if (cPizza.isSelected()) {
+                        order += 100;
+                    }
+                    if (cBurger.isSelected()) {
+                        order += 80;
+                    }
+                    if (cFries.isSelected()) {
+                        order += 65;
+                    }
+                    if (cSoftDrinks.isSelected()) {
+                        order += 55;
+                    }
+                    if (cTea.isSelected()) {
+                        order += 50;
+                    }
+                    if (cSundae.isSelected()) {
+                        order += 40;
+                    }
+                    if (rb5.isSelected()) {
+                        order -= 0.05 * order;
+                    }
+                    if (rb10.isSelected()) {
+                        order -= 0.10 * order;
+                    }
+                    if (rb15.isSelected()) {
+                        order -= 0.15 * order;
+                    }
+                    if (rbNone.isSelected()) {
+                        order -= 0;
+                    }
+
+                    if(order == 0) {
+                        throw new Exception("No items selected. Please select an order.");
+                    }
+                    if(!rbNone.isSelected() && !(rb5.isSelected() || rb10.isSelected() || rb15.isSelected())) {
+                        throw new Exception("Please select a discount option.");
+                    }
+
+                    JOptionPane.showMessageDialog(pnlMain, "The total price is Php " + String.format("%.2f", order));
+                } catch (Exception exception) {
+                    JOptionPane.showMessageDialog(pnlMain, exception.getMessage());
                 }
-                if (cBurger.isSelected()) {
-                    order += 80;
-                }
-                if (cFries.isSelected()) {
-                    order += 65;
-                }
-                if (cSoftDrinks.isSelected()) {
-                    order += 55;
-                }
-                if (cTea.isSelected()) {
-                    order += 50;
-                }
-                if (cSundae.isSelected()) {
-                    order += 40;
-                }
-                if (rb5.isSelected()) {
-                    order -= 0.05 * order;
-                }
-                if (rb10.isSelected()) {
-                    order -= 0.10 * order;
-                }
-                if (rb15.isSelected()) {
-                    order -= 0.15 * order;
-                }
-                if (rbNone.isSelected()) {
-                    order -= 0;
-                }
-                JOptionPane.showMessageDialog(pnlMain, "The total price is Php " + String.format("%.2f", order));
             }
         });
     }
