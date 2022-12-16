2d29dd4ff7d562adcc907f0776f075e52b54f410
Le Bronn <lebronn.samson@cit.edu>
2022-12-16 16:14:42 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,6 +1,8 @@
 package SimpleCalc;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimpleCalcGUI extends JFrame {
     private JPanel pnlMain;
@@ -14,9 +16,34 @@ public class SimpleCalcGUI extends JFrame {
     public static void main(String[] args) {
         SimpleCalcGUI app = new SimpleCalcGUI();
         app.setContentPane(app.pnlMain);
-        app.setSize(550, 300);
+        app.setSize(700, 500);
         app.setTitle("Simple Calculator");
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
         app.setVisible(true);
+    }
+
+    public SimpleCalcGUI() {
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double num1 = Double.parseDouble(tfNumber1.getText());
+                double num2 = Double.parseDouble(tfNumber2.getText());
+                String operation = cbOperations.getSelectedItem().toString();
+                switch(operation) {
+                    case "+":
+                        tfResult.setText(String.valueOf(num1 + num2));
+                        break;
+                    case "-":
+                        tfResult.setText(String.valueOf(num1 - num2));
+                        break;
+                    case "*":
+                        tfResult.setText(String.valueOf(num1 * num2));
+                        break;
+                    case "/":
+                        tfResult.setText(String.valueOf(num1 / num2));
+                        break;
+                }
+            }
+        });
     }
 }
