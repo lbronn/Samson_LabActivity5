e6fa1359baa0f01d912504ccfc99b81c06cb6b05
Le Bronn <lebronn.samson@cit.edu>
2022-12-16 17:27:22 +0800

    Complete FoodOrder

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,6 +1,8 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FoodOrderGUI extends JFrame {
     private JPanel pnlMain;
@@ -20,8 +22,48 @@ public class FoodOrderGUI extends JFrame {
         FoodOrderGUI app = new FoodOrderGUI();
         app.setContentPane(app.pnlMain);
         app.setSize(700, 700);
-        app.setTitle("Food Ordering");
+        app.setTitle("Food Ordering System");
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
         app.setVisible(true);
+    }
+
+    public FoodOrderGUI() {
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double order = 0;
+                if (cPizza.isSelected()) {
+                    order += 100;
+                }
+                if (cBurger.isSelected()) {
+                    order += 80;
+                }
+                if (cFries.isSelected()) {
+                    order += 65;
+                }
+                if (cSoftDrinks.isSelected()) {
+                    order += 55;
+                }
+                if (cTea.isSelected()) {
+                    order += 50;
+                }
+                if (cSundae.isSelected()) {
+                    order += 40;
+                }
+                if (rb5.isSelected()) {
+                    order -= 0.05 * order;
+                }
+                if (rb10.isSelected()) {
+                    order -= 0.10 * order;
+                }
+                if (rb15.isSelected()) {
+                    order -= 0.15 * order;
+                }
+                if (rbNone.isSelected()) {
+                    order -= 0;
+                }
+                JOptionPane.showMessageDialog(pnlMain, "The total price is Php " + String.format("%.2f", order));
+            }
+        });
     }
 }
